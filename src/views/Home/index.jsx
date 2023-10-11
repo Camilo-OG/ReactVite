81f538ba93d5d498e4d2001808ead9309732e610
camilo-OG <camilo.osorio.gsach.cl>
2023-10-11 17:20:42 -0300

    agregando paginacion

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -2,10 +2,12 @@ import { useRef, useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Events from "../../components/Events";
 import useEventsData from "../../hooks/useEventsData";
+import ReactPaginate from "react-paginate"
+import styles from "./Home.module.css"
 
 
 export default function Home() {
-  const {events,isLoading,error, fetchEvents} = useEventsData();
+  const {events,isLoading,error, fetchEvents, page} = useEventsData();
   const [searchTerm, setSearchTerm] = useState('');
   const containerRef = useRef();
 
@@ -18,13 +20,47 @@ export default function Home() {
     fetchEvents();
   }, [])
 
+  const handlePageClick = ({selected})=> {
+    fetchEvents(`&keyword=${searchTerm}&page=${selected}`)
+  }
+
+  const renderEvents = () => {
+    if(isLoading){
+      return <div>Cargando resultados</div>
+    }
+
+    if(error){
+      return <div> Ha ocurrido un error</div>
+    }
+    return (
+      <div>
+        <Events searchTerm={searchTerm} events={events}/>
+        <ReactPaginate
+          className={styles.pagination}
+          nextClassName={styles.next}
+          previousClassName={styles.previous}
+          pageClassName={styles.page}
+          activeClassName={styles.activePage}
+          disabledClassName={styles.disabledPage}
+          breakLabel="..."
+          nextLabel=" >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={page.totalPages}
+          previousLabel="< "
+          renderOnZeroPageCount={null}
+        />
+      </div>
+    )
+  }
+
  
   
   return (
     <>
       <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
-      {isLoading? <div>Cargando resultados</div> :  <Events searchTerm={searchTerm} events={events}/>}
-      {!! error && <div> Ha ocurrido un error</div>}
+      {renderEvents()}
+      
     </>
   )
   
